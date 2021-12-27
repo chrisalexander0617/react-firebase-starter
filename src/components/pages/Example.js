2778b2295bc6d26c94288a5bb642580084a207cc
Christopher <chrisalexander0617@gmail.com>
2021-12-27 09:02:24 -0500

    Added moduless

--- src/components/pages/Example.js
+++ src/components/pages/Example.js
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Content from '../molecules/Content'
+import { db } from '../firebase'
+import { collection, addDoc } from 'firebase/firestore'
 
-export default function Example(){
+const getVehicles = async () => {
+const data = await getDocs(vehiclesRef)
+setVehicles(data.docs.map( doc => (
+                {
+                    ...doc.data(), 
+                    id:doc.id 
+                }
+            )
+        )
+    )
+}
+
+function Example(){
+    const [yourDocs, loadYourDocs] = useState(null)
+
+    useEffect(() => {
+        // [good practice] preventing memory leak
+        let mounted = false
+
+        return () => { mounted = true }
+    }, [])
+
     return  <Content title="Example Queries" text="take a look" />
 }
+
+export default Example
