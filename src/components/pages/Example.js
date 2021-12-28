f80423110f2377f4d7d7960cea296a7c04f52b8f
Christopher <chrisalexander0617@gmail.com>
2021-12-27 19:30:00 -0500

    Added code to prevent memory leaks

--- src/components/pages/Example.js
+++ src/components/pages/Example.js
@@ -3,35 +3,42 @@ import Content from '../molecules/Content'
 import { db } from '../../firebase-config'
 import { collection, getDocs } from 'firebase/firestore'
 
-function Example(){
-    const isMounted = useRef(false)
+//  You can change the variable names to whatever you like as 
+// long as you can make a reference to the collections object
 
+function Example(){
     const [vehicles, setVehicles] = useState([])
     const vehiclesRef = collection(db, 'vehicles')
 
     useEffect(() => {
-        isMounted.current = true
+
+        // This variable prevents memory leak when using async calls
+        // https://www.loginradius.com/blog/async/how-to-fix-memory-leaks-in-react/
+        
+        let isMounted = true
 
         const getVehicles = async () => {
             const data = await getDocs(vehiclesRef)
-            setVehicles(data.docs.map( doc => (
-                        {
-                            ...doc.data(), 
-                            id:doc.id 
-                        }
+            if(isMounted){
+                setVehicles(data.docs.map( doc => (
+                            {
+                                ...doc.data(), 
+                                id:doc.id 
+                            }
+                        )
                     )
                 )
-            )
+            }
         }
 
         getVehicles()
 
         console.log(vehicles)
 
-        return () => { isMounted.current = false }
+        return () => { isMounted = false }
     }, [])
 
-return  <Content title="Example Queries" text="Check the console to see data" />
+    return  <Content title="Example Queries" text="Check the console to see data" />
 }
 
 export default Example
